7e89f496acd878c66b6ff84c4c654d344ca98eae
doannguyen263 <doannguyen263@gmail.com>
2021-10-21 15:53:20 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,21 +12,18 @@ $(document).ready(function(){
     //-------------------------------------------------
     // Menu
     //-------------------------------------------------
+    $('.nav__mobile--close').click(function(e){
+        $('.nav__mobile').removeClass('active')
+        $('body').removeClass('modal-open')
+    });
     $('.menu-mb__btn').click(function(e){
         e.preventDefault()
-        if($('.menu-mb__btn').hasClass('active')){
-
-            $('body').removeClass('modal-open')
-            $('.menu-mb__btn').removeClass('active')
-            $('.nav__mobile').removeClass('active')
-
-        } else {
-            $('body').addClass('modal-open')
-            $('.menu-mb__btn').addClass('active')
-            $('.nav__mobile').addClass('active')
-        }
+        $('body').addClass('modal-open')
+        // $('.menu-mb__btn').addClass('active')
+        $('.nav__mobile').addClass('active')
     });
 
+    var back_to_top=$(".scrollTop"),offset=220,duration=500;$(window).scroll(function(){$(this).scrollTop()>offset?back_to_top.addClass("active"):back_to_top.removeClass("active")}),$(document).on("click",".scrollTop",function(o){return o.preventDefault(),$("html, body").animate({scrollTop:0},duration),!1});
 
 });
 
